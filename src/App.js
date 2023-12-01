b4a99bd76c2e6fd7fe236370c62fa74c3af55dda
tomohiro1024 <gongyezhihong732@gmail.com>
2023-12-01 10:31:40 +0900

    ImageGallery

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import ImageGallery from "./ImageGallery";
 
 function App() {
-  return <div className="App"></div>
+  return (
+  <div className="container">
+    <h2>My Pixabay</h2>
+    <form>
+      <input type='text' placeholder='画像を探す'></input>
+    </form>
+    <ImageGallery />
+  </div>
+  )
 }
 
 export default App;
---
+++ src/ImageGallery.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ImageGallery = () => {
+  return (
+    <div>
+      <div className="images-wrapper"></div>
+    </div>
+  )
+}
+
+export default ImageGallery
