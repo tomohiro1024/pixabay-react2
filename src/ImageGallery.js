33a7125ffe732b8852cc423c049aa483e686e1fc
tomohiro1024 <gongyezhihong732@gmail.com>
2023-12-02 13:51:58 +0900

    画像の表示

--- src/ImageGallery.js
+++ src/ImageGallery.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import "./ImageGrallery.css";
 
 const ImageGallery = () => {
   return (
     <div>
-      <div className="images-wrapper"></div>
+      <div className="images-wrapper">
+        <div className="image">
+          <img src='https://pixabay.com/get/g064ecffc7be6323a4b1fc8e5c3985c5df85ff31861f16642711a49ae5090b4e98df1c51a30dae097132222ecb7cde3a21c678852c0b9235f4041842c8e65658e_1280.jpg'
+               alt=''/>
+        </div>
+        <div className="image">
+          <img src='https://pixabay.com/get/g064ecffc7be6323a4b1fc8e5c3985c5df85ff31861f16642711a49ae5090b4e98df1c51a30dae097132222ecb7cde3a21c678852c0b9235f4041842c8e65658e_1280.jpg'
+               alt=''/>
+        </div>
+        <div className="image">
+          <img src='https://pixabay.com/get/g064ecffc7be6323a4b1fc8e5c3985c5df85ff31861f16642711a49ae5090b4e98df1c51a30dae097132222ecb7cde3a21c678852c0b9235f4041842c8e65658e_1280.jpg'
+               alt=''/>
+        </div>
+        <div className="image">
+          <img src='https://pixabay.com/get/g064ecffc7be6323a4b1fc8e5c3985c5df85ff31861f16642711a49ae5090b4e98df1c51a30dae097132222ecb7cde3a21c678852c0b9235f4041842c8e65658e_1280.jpg'
+               alt=''/>
+        </div>
+        <div className="image">
+          <img src='https://pixabay.com/get/g064ecffc7be6323a4b1fc8e5c3985c5df85ff31861f16642711a49ae5090b4e98df1c51a30dae097132222ecb7cde3a21c678852c0b9235f4041842c8e65658e_1280.jpg'
+               alt=''/>
+        </div>
+        <div className="image">
+          <img src='https://pixabay.com/get/g064ecffc7be6323a4b1fc8e5c3985c5df85ff31861f16642711a49ae5090b4e98df1c51a30dae097132222ecb7cde3a21c678852c0b9235f4041842c8e65658e_1280.jpg'
+               alt=''/>
+        </div>
+      </div>
     </div>
   )
 }
