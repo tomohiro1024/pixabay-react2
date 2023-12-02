bbdbec1e7fc8fdd35d6e6af74275be0fecb5acef
tomohiro1024 <gongyezhihong732@gmail.com>
2023-12-02 16:53:34 +0900

    useRef()

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import ImageGallery from "./ImageGallery";
+import { useRef, useState } from 'react';
 
 function App() {
+  const [inputText, setInputText] = useState("")
+  const ref = useRef()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(ref.current.value)
+  }
+
   return (
   <div className="container">
     <h2>My Pixabay</h2>
-    <form>
-      <input type='text' placeholder='画像を探す'></input>
+    <form onSubmit={(e) => handleSubmit(e)}>
+      <input type='text' placeholder='画像を探す' ref={ref}></input>
     </form>
     <ImageGallery />
   </div>
